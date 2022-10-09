ef91cb8bf506ed951a14186a0ad4f614b204e201
NickSettler <moiseevnikita14@gmail.com>
2022-10-09 12:46:38 +0200

    (ci) adds lexical analyzer tests

--- tests/lexical_fsm_test.cpp
+++ tests/lexical_fsm_test.cpp
@@ -1,33 +1,134 @@
 #include <gtest/gtest.h>
+#include <stdio.h>
 #include <stdlib.h>
+
 extern "C" {
-    #include "../src/lexical_fsm.h"
-    #include "../src/lexical_fsm.c"
-    #include "../src/lexical_token.h"
-    #include "../src/lexical_token.c"
+#include "../src/lexical_fsm.h"
+#include "../src/lexical_fsm.c"
+#include "../src/lexical_token.h"
+#include "../src/lexical_token.c"
 }
 
-TEST(LexicalAnalyzer, TypedIntVariableDeclaration) {
-    lexical_token_stack_t *stack = lexical_token_stack_init();
+namespace ifj {
+    namespace tests {
+        namespace {
+            class LexicalAnalyzerTest : public ::testing::Test {
+            protected:
+                lexical_token_stack_t *token_stack;
+                string_t *token;
+                FILE *fd{};
 
-    string_t *token = string_init("");
-    LEXICAL_FSM_TOKENS token_type = get_next_token(token);
+                LexicalAnalyzerTest() {
+                    token_stack = lexical_token_stack_init();
+                    token = string_init("");
+                }
 
-    while (token_type != END_OF_FILE) {
-        string_t *token_value = string_init(token->value);
-        lexical_token_stack_push(stack, (lexical_token_t) {token_type, token_value->value});
-        token_type = get_next_token(token);
+                ~LexicalAnalyzerTest() override {
+                    fclose(fd);
+                }
+
+                void SetUp() override {
+                }
+
+                void IsStackCorrect(char *input, int n, ...) {
+                    lexical_token_stack_t *correct_stack = lexical_token_stack_init();
+                    va_list tokens;
+                    va_start(tokens, n);
+
+                    for (int i = 0; i < n; i++) {
+                        lexical_token_t arg_token = va_arg(tokens, lexical_token_t);
+                        lexical_token_stack_push(correct_stack, arg_token);
+                    }
+
+                    va_end(tokens);
+
+                    fd = fmemopen(input, strlen(input), "r");
+                    LEXICAL_FSM_TOKENS token_type = get_next_token(fd, token);
+
+                    while (token_type != END_OF_FILE) {
+                        string_t *token_value = string_init(token->value);
+                        lexical_token_stack_push(token_stack, (lexical_token_t) {token_type, token_value->value});
+                        token_type = get_next_token(fd, token);
+                    }
+
+                    while (!lexical_token_stack_empty(token_stack)) {
+                        lexical_token_t input_token = lexical_token_stack_pop(token_stack);
+                        lexical_token_t correct_token = lexical_token_stack_pop(correct_stack);
+                        EXPECT_EQ(input_token.type, correct_token.type);
+                        EXPECT_STREQ(input_token.value, correct_token.value);
+                    }
+                }
+            };
+
+            TEST_F(LexicalAnalyzerTest, VariableDeclaration) {
+                IsStackCorrect("int $a = 5;", 5,
+                               (lexical_token_t) {KEYWORD_INTEGER, "int"},
+                               (lexical_token_t) {IDENTIFIER, "$a"},
+                               (lexical_token_t) {ASSIGN, "="},
+                               (lexical_token_t) {INTEGER, "5"},
+                               (lexical_token_t) {SEMICOLON, ";"}
+                );
+                IsStackCorrect("?int $a = 5;", 5,
+                               (lexical_token_t) {KEYWORD_INTEGER, "?int"},
+                               (lexical_token_t) {IDENTIFIER, "$a"},
+                               (lexical_token_t) {ASSIGN, "="},
+                               (lexical_token_t) {INTEGER, "5"},
+                               (lexical_token_t) {SEMICOLON, ";"}
+                );
+                IsStackCorrect("$a = 5;", 4,
+                               (lexical_token_t) {IDENTIFIER, "$a"},
+                               (lexical_token_t) {ASSIGN, "="},
+                               (lexical_token_t) {INTEGER, "5"},
+                               (lexical_token_t) {SEMICOLON, ";"}
+                );
+                IsStackCorrect("string $a = \"abcdef\";", 5,
+                               (lexical_token_t) {KEYWORD_STRING, "string"},
+                               (lexical_token_t) {IDENTIFIER, "$a"},
+                               (lexical_token_t) {ASSIGN, "="},
+                               (lexical_token_t) {STRING, "\"abcdef\""},
+                               (lexical_token_t) {SEMICOLON, ";"}
+                );
+            }
+
+
+            TEST_F(LexicalAnalyzerTest, VariablesOperators) {
+                IsStackCorrect("$a == $b;", 4,
+                               (lexical_token_t) {IDENTIFIER, "$a"},
+                               (lexical_token_t) {EQUAL, "=="},
+                               (lexical_token_t) {IDENTIFIER, "$b"},
+                               (lexical_token_t) {SEMICOLON, ";"}
+                );
+                IsStackCorrect("$a <> $b;", 4,
+                               (lexical_token_t) {IDENTIFIER, "$a"},
+                               (lexical_token_t) {NOT_EQUAL, "<>"},
+                               (lexical_token_t) {IDENTIFIER, "$b"},
+                               (lexical_token_t) {SEMICOLON, ";"}
+                );
+                IsStackCorrect("$a < $b;", 4,
+                               (lexical_token_t) {IDENTIFIER, "$a"},
+                               (lexical_token_t) {LESS, "<"},
+                               (lexical_token_t) {IDENTIFIER, "$b"},
+                               (lexical_token_t) {SEMICOLON, ";"}
+                );
+                IsStackCorrect("$a > $b;", 4,
+                               (lexical_token_t) {IDENTIFIER, "$a"},
+                               (lexical_token_t) {GREATER, ">"},
+                               (lexical_token_t) {IDENTIFIER, "$b"},
+                               (lexical_token_t) {SEMICOLON, ";"}
+                );
+                IsStackCorrect("$a <= $b;", 4,
+                               (lexical_token_t) {IDENTIFIER, "$a"},
+                               (lexical_token_t) {LESS_EQUAL, "<="},
+                               (lexical_token_t) {IDENTIFIER, "$b"},
+                               (lexical_token_t) {SEMICOLON, ";"}
+                );
+                IsStackCorrect("$a >= $b;", 4,
+                               (lexical_token_t) {IDENTIFIER, "$a"},
+                               (lexical_token_t) {GREATER_EQUAL, ">="},
+                               (lexical_token_t) {IDENTIFIER, "$b"},
+                               (lexical_token_t) {SEMICOLON, ";"}
+                );
+            }
+        }
     }
-
-    lexical_token_t l_token_1 = lexical_token_stack_pop(stack);
-    lexical_token_t l_token_2 = lexical_token_stack_pop(stack);
-    lexical_token_t l_token_3 = lexical_token_stack_pop(stack);
-    lexical_token_t l_token_4 = lexical_token_stack_pop(stack);
-    lexical_token_t l_token_5 = lexical_token_stack_pop(stack);
-
-    EXPECT_STREQ(l_token_1.value, ";");
-    EXPECT_STREQ(l_token_2.value, "105");
-    EXPECT_STREQ(l_token_3.value, "=");
-    EXPECT_STREQ(l_token_4.value, "$abc");
-    EXPECT_STREQ(l_token_5.value, "int");
 }
