f6ef8e23034cb507568c25151db40d23b846b9c1
Defancet <gmamyako@gmail.com>
2022-10-16 01:35:57 +0200

    (refactor) refactoring code

--- src/code_generator.c
+++ src/code_generator.c
@@ -1,122 +1,374 @@
+/**
+ * Imperative IFJ22 language compiler implementation
+ * @author xkalut00, Maksim Kalutski
+ * @file   code_generator.c
+ * @brief  Code generator source file
+ * @date   ??.10.2022
+ */
+
 #include "code_generator.h"
 
-#include <stdio.h>
-#include <stdbool.h>
-
-// Assign a value to a variable
-void generate_move(char *parameter_frame, char *parameter, char *variable_frame, char *variable) {
-    fprintf(fd, "MOVE %s%s %s%s\n", parameter_frame, parameter, variable_frame, variable);
+void generate_move(char *variable_frame, char *variable, char *symbol_frame, char *symbol) {
+    fprintf(fd, "MOVE %s@%s %s@%s\n", variable_frame, variable, symbol_frame, symbol);
 }
 
-// Create a new temporary frame
 void generate_create_frame() {
     fprintf(fd, "CREATEFRAME\n");
 }
 
-// Push a frame to the stack
 void generate_push_frame() {
     fprintf(fd, "PUSHFRAME\n");
 }
 
-// Pop a frame from the stack
 void generate_pop_frame() {
     fprintf(fd, "POPFRAME\n");
 }
 
-// Define a new variable in the frame
 void generate_declaration(char *variable_frame, char *variable) {
-    fprintf(fd, "DEFVAR %s%s\n", variable_frame, variable);
+    fprintf(fd, "DEFVAR %s@%s\n", variable_frame, variable);
 }
 
-// Jump on a sign with return support
 void generate_call(char *label) {
-    fprintf(fd, "CALL $%s\n", label);
+    fprintf(fd, "CALL %s\n", label);
 }
 
-// Put the value on the top of the stack
-void generate_push(char *variable_frame, char *variable) {
-    fprintf(fd, "PUSHS %s%s\n", variable_frame, variable);
+void generate_add_on_top(char *type, char *variable) {
+    fprintf(fd, "PUSHS %s@%s\n", type, variable);
 }
 
-// Sum of two numerical values
-void generate_add(char *result, char *symbol1, char *symbol2) {
-    fprintf(fd, "ADD LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+void generate_pop_from_top(char *variable_frame, char *variable) {
+    fprintf(fd, "POPS %s@%s\n", variable_frame, variable);
 }
 
-// Subtraction of two numerical values
-void generate_sub(char *result, char *symbol1, char *symbol2) {
-    fprintf(fd, "SUB LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+void generate_clear_stack() {
+    fprintf(fd, "LABEL clear_stack\n");
+    fprintf(fd, "MOVE GT@clear_var bool@true\n");
+    fprintf(fd, "JUMPIFEQ clear_stack_end GF@clear_top_var bool@true\n");
+    fprintf(fd, "POPS GT@_clear_stack\n");
+    fprintf(fd, "JUMP clear_stack\n");
+    fprintf(fd, "RETURN\n");
 }
 
-// Multiplication of two numerical values
-void generate_mul(char *result, char *symbol1, char *symbol2) {
-    fprintf(fd, "MUL LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+void generate_operation(instructions_t instruction, char *result, char *symbol1, char *symbol2) {
+    switch (instruction) {
+        case ADD:
+            fprintf(fd, "ADD LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case SUB:
+            fprintf(fd, "SUB LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case MUL:
+            fprintf(fd, "MUL LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case IMUL:
+            fprintf(fd, "IMUL LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case DIV:
+            fprintf(fd, "DIV LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case IDIV:
+            fprintf(fd, "IDIV LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case LT:
+            fprintf(fd, "LT LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case GT:
+            fprintf(fd, "GT LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case EQ:
+            fprintf(fd, "EQ LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case NOTLT:
+            fprintf(fd, "LT LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            fprintf(fd, "NOT LF@%s LF@%s\n", result, result);
+            break;
+        case NOTGT:
+            fprintf(fd, "GT LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            fprintf(fd, "NOT LF@%s LF@%s\n", result, result);
+            break;
+        case NOTEQ:
+            fprintf(fd, "EQ LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            fprintf(fd, "NOT LF@%s LF@%s\n", result, result);
+            break;
+        case AND:
+            fprintf(fd, "AND LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case OR:
+            fprintf(fd, "OR LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case NOT:
+            fprintf(fd, "NOT LF@%s LF@%s\n", result, symbol1);
+            break;
+        default:
+            fprintf(stderr, "Unknown operation");
+    }
 }
 
-// Division of two numerical values
-void generate_idiv(char *result, char *symbol1, char *symbol2) {
-    fprintf(fd, "IDIV LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+void generate_stack_operation(stack_instructions_t instruction, char *result, char *symbol1, char *symbol2) {
+    switch (instruction) {
+        case ADDS:
+            fprintf(fd, "ADDS LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case SUBS:
+            fprintf(fd, "SUBS LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case MULS:
+            fprintf(fd, "MULS LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case DIVS:
+            fprintf(fd, "DIVS LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case IDIVS:
+            fprintf(fd, "IDIVS LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        default:
+            fprintf(stderr, "Unknown operation");
+    }
 }
 
-// Compare two values for lower that
-void generate_lt(char *result, char *symbol1, char *symbol2) {
-    fprintf(fd, "LT LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+void generate_string_relational_operation(instructions_t instruction, char *result, char *symbol1, char *symbol2) {
+    switch (instruction) {
+        case CONCAT:
+            fprintf(fd, "CONCAT LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case STRLEN:
+            fprintf(fd, "STRLEN LF@%s LF@%s\n", result, symbol1);
+            break;
+        case GETCHAR:
+            fprintf(fd, "GETCHAR LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case SETCHAR:
+            fprintf(fd, "SETCHAR LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case LT:
+            fprintf(fd, "LT LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case GT:
+            fprintf(fd, "GT LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case EQ:
+            fprintf(fd, "EQ LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            break;
+        case NOTLT:
+            fprintf(fd, "LT LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            fprintf(fd, "NOT LF@%s LF@%s\n", result, result);
+            break;
+        case NOTGT:
+            fprintf(fd, "GT LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            fprintf(fd, "NOT LF@%s LF@%s\n", result, result);
+            break;
+        case NOTEQ:
+            fprintf(fd, "EQ LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+            fprintf(fd, "NOT LF@%s LF@%s\n", result, result);
+            break;
+        default:
+            fprintf(stderr, "Unknown operation");
+    }
 }
 
-// Compare two values for greater than
-void generate_gt(char *result, char *symbol1, char *symbol2) {
-    fprintf(fd, "GT LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+void generate_int_to_float() {
+    fprintf(fd, "LABEL float2int\n");
+    fprintf(fd, "CREATEFRAME\n");
+    fprintf(fd, "PUSHFRAME\n");
+    fprintf(fd, "DEFVAR LF@retval1\n");
+    fprintf(fd, "MOVE LF@retval1 nil@nil\n");
+    fprintf(fd, "DEFVAR LF@float2int\n");
+    fprintf(fd, "MOVE LF@float2int LF@1\n");
+    fprintf(fd, "INT2FLOAT LF@retval1 LF@float2int\n");
+    fprintf(fd, "POPFRAME\n");
+    fprintf(fd, "RETURN\n");
 }
 
-// Compare two values for equality
-void generate_eq(char *result, char *symbol1, char *symbol2) {
-    fprintf(fd, "EQ LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+void generate_float_to_int() {
+    fprintf(fd, "LABEL int2float\n");
+    fprintf(fd, "CREATEFRAME\n");
+    fprintf(fd, "PUSHFRAME\n");
+    fprintf(fd, "DEFVAR LF@retval1\n");
+    fprintf(fd, "MOVE LF@retval1 nil@nil\n");
+    fprintf(fd, "DEFVAR LF@int2float\n");
+    fprintf(fd, "MOVE LF@int2float LF@1\n");
+    fprintf(fd, "FLOAT2INT LF@retval1 LF@int2float\n");
+    fprintf(fd, "POPFRAME\n");
+    fprintf(fd, "RETURN\n");
 }
 
-// Compare two values with logical AND
-void generate_and(char *result, char *symbol1, char *symbol2) {
-    fprintf(fd, "AND LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+void generate_int_to_char() {
+    fprintf(fd, "LABEL int2char\n");
+    fprintf(fd, "PUSHFRAME\n");
+    fprintf(fd, "DEFVAR LF@retval1\n");
+    fprintf(fd, "MOVE LF@retval1 nil@nil\n");
+    fprintf(fd, "DEFVAR LF@i\n");
+    fprintf(fd, "MOVE LF@i LF@%1\n");
+    fprintf(fd, "DEFVAR LF@i\n");
+    fprintf(fd, "MOVE LF@tmp nil@nil\n");
+    fprintf(fd, "TYPE LF@tmp LF@int\n");
+    fprintf(fd, "JUMPIFEQ error_label LF@tmp string@nil\n");
+    fprintf(fd, "LT LF@tmp LF@i int@0\n");
+    fprintf(fd, "JUMPIFEQ nil_return LF@tmp bool@true\n");
+    fprintf(fd, "GT LF@tmp LF@i int@255\n");
+    fprintf(fd, "JUMPIFEQ nil_return LF@tmp bool@true\n");
+    fprintf(fd, "INT2CHAR LF@retval1 LF@int\n");
+    fprintf(fd, "LABEL nil_return\n");
+    fprintf(fd, "POPFRAME\n");
+    fprintf(fd, "RETURN\n");
 }
 
-//  Compare two values with logical OR
-void generate_or(char *result, char *symbol1, char *symbol2) {
-    fprintf(fd, "OR LF@%s LF@%s LF@%s\n", result, symbol1, symbol2);
+void generate_string_to_int() {
+    fprintf(fd, "LABEL stri2int\n");
+    fprintf(fd, "CREATEFRAME\n");
+    fprintf(fd, "PUSHFRAME\n");
+    fprintf(fd, "DEFVAR LF@s\n");
+    fprintf(fd, "MOVE LF@s LF@1\n");
+    fprintf(fd, "DEFVAR LF@i\n");
+    fprintf(fd, "MOVE LF@i LF@2\n");
+    fprintf(fd, "DEFVAR LF@lenght\n");
+    fprintf(fd, "MOVE LF@length nil@nil\n");
+    fprintf(fd, "DEFVAR LF@tmp\n");
+    fprintf(fd, "MOVE LF@tmp nil@nil\n");
+    fprintf(fd, "TYPE LF@tmp LF@s\n");
+    fprintf(fd, "JUMPIFEQ error_label LF@tmp string@nil\n");
+    fprintf(fd, "TYPE LF@tmp LF@i\n");
+    fprintf(fd, "JUMPIFEQ error_label LF@tmp string@nil\n");
+    fprintf(fd, "STRLEN LF@length LF@s\n");
+    fprintf(fd, "LT LF@tmp LF@i int@0\n");
+    fprintf(fd, "JUMPIFEQ nil_return LF@tmp bool@true\n");
+    fprintf(fd, "GT LF@tmp LF@i LF@length\n");
+    fprintf(fd, "JUMPIFEQ nil_return LF@tmp bool@true\n");
+    fprintf(fd, "STRI2INT LF@retval1 LF@s LF@i\n");
+    fprintf(fd, "LABEL nil_return\n");
+    fprintf(fd, "POPFRAME\n");
+    fprintf(fd, "RETURN\n");
 }
 
-// Compare two values with logical NOT
-void generate_not(char *result, char *symbol) {
-    fprintf(fd, "NOT LF@%s LF@%s\n", result, symbol);
+void generate_conversion_operation(stack_conversion_t instruction) {
+    switch (instruction) {
+        case INT2FLOATS:
+            fprintf(fd, "INT2FLOATS\n");
+            break;
+        case FLOAT2INTS:
+            fprintf(fd, "FLOAT2INTS\n");
+            break;
+        case INT2CHARS:
+            fprintf(fd, "INT2CHARS\n");
+            break;
+        case STRI2INTS:
+            fprintf(fd, "STRI2INTS\n");
+            break;
+        default:
+            fprintf(stderr, "Unknown operation");
+    }
+}
+
+void generate_reads() {
+    fprintf(fd, "LABEL reads\n");
+    fprintf(fd, "PUSHFRAME\n");
+    fprintf(fd, "DEFVAR LF@retval1\n");
+    fprintf(fd, "MOVE LF@retval1\n");
+    fprintf(fd, "READ LF@retval1 string\n");
+    fprintf(fd, "POPFRAME\n");
+    fprintf(fd, "RETURN\n");
+}
+
+void generate_readi() {
+    fprintf(fd, "LABEL readi\n");
+    fprintf(fd, "PUSHFRAME\n");
+    fprintf(fd, "DEFVAR LF@retval1\n");
+    fprintf(fd, "MOVE LF@retval1\n");
+    fprintf(fd, "READ LF@retval1 int\n");
+    fprintf(fd, "POPFRAME\n");
+    fprintf(fd, "RETURN\n");
+}
+
+void generate_readf() {
+    fprintf(fd, "LABEL readf\n");
+    fprintf(fd, "PUSHFRAME\n");
+    fprintf(fd, "DEFVAR LF@retval1\n");
+    fprintf(fd, "MOVE LF@retval1\n");
+    fprintf(fd, "READ LF@retval1 float\n");
+    fprintf(fd, "POPFRAME\n");
+    fprintf(fd, "RETURN\n");
+}
+
+void generate_write() {
+    fprintf(fd, "LABEL write\n");
+    fprintf(fd, "PUSHFRAME\n");
+    fprintf(fd, "DEFVAR LF@write_to\n");
+    fprintf(fd, "DEFVAR LF@check_for_nil\n");
+    fprintf(fd, "POPS LF@write_to\n");
+    fprintf(fd, "TYPE LF@check_for_nil LF@write_to\n");
+    fprintf(fd, "JUMPIFEQ its_nil\n");
+    fprintf(fd, "WRITE LF@write_to\n");
+    fprintf(fd, "POPFRAME\n");
+    fprintf(fd, "RETURN\n");
+    fprintf(fd, "LABEL its_nil\n");
+    fprintf(fd, "WRITE string@nil\n");
+    fprintf(fd, "POPFRAME\n");
+    fprintf(fd, "RETURN");
+}
+
+void generate_substr() {
+    fprintf(fd, "LABEL substr\n");
+    fprintf(fd, "PUSHFRAME\n");
+    fprintf(fd, "DEFVAR LF@retval1\n");
+    fprintf(fd, "MOVE LF@retval1 string@\n");
+
+    fprintf(fd, "DEFVAR LF@s\n");           // ("s", 1)
+    fprintf(fd, "MOVE LF@s int@1\n");
+
+    fprintf(fd, "DEFVAR LF@i\n");           // ("i", 2)
+    fprintf(fd, "MOVE LF@i int@2\n");
+
+    fprintf(fd, "DEFVAR LF@j\n");           // ("j", 3)
+    fprintf(fd, "MOVE LF@j int@3\n");
+
+    fprintf(fd, "DEFVAR LF@tmp\n");         // ("tmp",)
+    fprintf(fd, "MOVE LF@tmp nil@nil\n");
+
+    fprintf(fd, "DEFVAR LF@length\n");      // ("length")
+    fprintf(fd, "MOVE LF@length nil@nil\n");
+
+    fprintf(fd, "DEFVAR LF@sing LF@s\n");   // ("sing")
+    fprintf(fd, "MOVE LF@sing nil@nil\n");
+
+    fprintf(fd, "TYPE LF@tmp LF@s\n");
+    fprintf(fd, "JUMPIFEQ error LF@tmp string@nil\n");
+    fprintf(fd, "TYPE LF@tmp LF@i\n");
+    fprintf(fd, "JUMPIFEQ error LF@tmp string@nil\n");
+    fprintf(fd, "TYPE LF@tmp LF@j\n");
+    fprintf(fd, "JUMPIFEQ error LF@tmp string@nil\n");
+
+    fprintf(fd, "STRLEN LF@length LF@s\n");
+    fprintf(fd, "LT LF@tmp LF@i int@1\n");
+    fprintf(fd, "JUMPIFEQ return LF@tmp bool@false\n");
+    fprintf(fd, "GT LF@tmp LF@j LF@lenght\n");
+    fprintf(fd, "JUMPIFEQ return LF@tmp bool@false\n");
+
+    fprintf(fd, "GT LF@tmp LF@j LF@length\n");
+    fprintf(fd, "JUMPIFEQ return LF@tmp bool@true\n");
+    fprintf(fd, "LT LF@tmp LF@j int@1\n");
+    fprintf(fd, "JUMPIFEQ return LF@tmp bool@true\n");
+
+    fprintf(fd, "GT LF@tmp LF@i LF@j\n");
+    fprintf(fd, "JUMPIFEQ return LF@tmp bool@true\n");
+
+    fprintf(fd, "SUB LF@i LF@i int@1\n");
+    fprintf(fd, "SUB LF@j LF@j int@1\n");
+
+    fprintf(fd, "LABEL loop\n");
+    fprintf(fd, "GETCHAR LF@sing LF@s LF@i\n");
+    fprintf(fd, "CONCAT LF@retval1 LF@retval1 LF@sing\n");
+
+    fprintf(fd, "ADD LF@i LF@i int@1\n");
+    fprintf(fd, "LT LF@tmp LF@i LF@j\n");
+    fprintf(fd, "JUMPIFEQ return LF@tmp bool@true\n");
+    fprintf(fd, "JUMP loop\n");
+
+    fprintf(fd, "LABEL return\n");
+    fprintf(fd, "POPFRAME\n");
+    fprintf(fd, "RETURN\n");
 }
 
 void generate_end() {
-    printf("\nPOPFRAME");
-    printf("\nRETURN");
+    fprintf(fd, "POPFRAME\n");
+    fprintf(fd, "RETURN\n");
 }
-
-int test_code_generator() {
-    fd = fopen("test.txt", "w");
-    generate_move("GF", "a", "GF", "b");
-    generate_create_frame();
-    generate_push_frame();
-    generate_pop_frame();
-    generate_declaration("GF", "c");
-    generate_call("test");
-    generate_push("GF", "a");
-    generate_add("c", "a", "b");
-    generate_sub("c", "a", "b");
-    generate_mul("c", "a", "b");
-    generate_idiv("c", "a", "b");
-    generate_lt("c", "a", "b");
-    generate_gt("c", "a", "b");
-    generate_eq("c", "a", "b");
-    generate_and("c", "a", "b");
-    generate_or("c", "a", "b");
-    generate_not("c", "a");
-    fclose(fd);
-    return 0;
-}
-
-
-
-
-
--- src/code_generator.h
+++ src/code_generator.h
@@ -1,21 +1,229 @@
+/**
+ * Imperative IFJ22 language compiler implementation
+ * @author xkalut00, Maksim Kalutski
+ * @file   code_generator.h
+ * @brief  Code generator header file
+ * @date   ??.10.2022
+ */
+
 #ifndef IFJ_PROJ_2022_CODE_GENERATOR_H
 #define IFJ_PROJ_2022_CODE_GENERATOR_H
 
-FILE* fd; //file for testing
+#include <stdio.h>
 
-void generate_move(char *parameter_frame, char *parameter, char *variable_frame, char *variable);
+/**
+ * @brief file for writing the code
+ */
+FILE *fd;
+
+/**
+ * @brief structure with instructions for switch cases
+ */
+typedef enum {
+    ADD,
+    SUB,
+    MUL,
+    IMUL,
+    DIV,
+    IDIV,
+    LT,
+    GT,
+    EQ,
+    NOTLT,
+    NOTGT,
+    NOTEQ,
+    AND,
+    OR,
+    NOT,
+    CONCAT,
+    STRLEN,
+    GETCHAR,
+    SETCHAR,
+} instructions_t;
+
+/**
+ * @brief structure with stack instructions for switch cases
+ */
+typedef enum {
+    ADDS,
+    SUBS,
+    MULS,
+    DIVS,
+    IDIVS,
+    ANDS,
+    ORS,
+    NOTS,
+} stack_instructions_t;
+
+/**
+ * @brief structure with stack conversion operations for switch cases
+ */
+typedef enum {
+    INT2FLOATS,
+    FLOAT2INTS,
+    INT2CHARS,
+    STRI2INTS,
+} stack_conversion_t;
+
+/**
+ * @brief generating a value assignment to a variable
+ * @param *variable_frame variable frame
+ * @param *variable variable name
+ * @param *symbol_frame symbol frame
+ * @param *symbol symbol name
+ */
+void generate_move(char *variable_frame, char *variable, char *symbol_frame, char *symbol);
+
+/**
+ * @brief generating a new temporary frame
+ */
 void generate_create_frame();
+
+/**
+ * @brief generating a temporary frame move to the frame stack
+ */
 void generate_push_frame();
+
+/**
+ * @brief generating a current frame move to the temporary
+ */
 void generate_pop_frame();
-void generate_declaration(char* variable_frame, char* variable);
+
+/**
+ * @brief generating declaration of a new variable in the frame
+ * @param *variable_frame variable frame
+ * @param *variable variable name
+ */
+void generate_declaration(char *variable_frame, char *variable);
+
+/**
+ * @brief generating a jump on the label
+ * @param *label specified label
+ */
 void generate_call(char *label);
-void generate_push(char* variable_frame, char* variable);
-void generate_add(char* result, char* symbol1, char* symbol2);
-void generate_sub (char* result, char* symbol1, char* symbol2);
-void generate_mul (char* result, char* symbol1, char* symbol2);
-void generate_idiv (char* result, char* symbol1, char* symbol2);
-void generate_lt (char* result, char* symbol1, char* symbol2);
-void generate_gt (char* result, char* symbol1, char* symbol2);
-void generate_eq (char* result, char* symbol1, char* symbol2);
+
+/**
+ * @brief placing the value on top of the data stack
+ * @param *type variable type
+ * @param *variable variable name
+ */
+void generate_add_on_top(char *type, char *variable);
+
+/**
+ * @brief placing the value on top of the data stack
+ * @param *type variable type
+ * @param *variable variable name
+ */
+void generate_pop_from_top(char *variable_frame, char *variable);
+
+/**
+ * @brief erases the data stack
+ */
+void generate_clear_stack();
+
+/**
+ * @brief function for choosing a float operation using a switch case
+ *        ADD - sums symbol1 and symbol2 and stores the result in result
+ *        SUB - subtracts symbol2 from symbol1 and stores the result in result
+ *        MUL - multiplies symbol1 and symbol2 and stores the result in result
+ *        DIV - divides symbol1 by symbol2 and stores the result in result
+ *        LT  - compares for lowness symbol1 and symbol2 and stores the result in result
+ *        GT  - compares for greatness symbol1 and symbol2 and stores the result in result
+ *        EQ  - compares for equality symbol1 and symbol2 and stores the result in result
+ *        NOTLT/NOTGT/NOTEQ - negation of LT/GT/EQ
+ * @param *operation float operation
+ * @param *result result
+ * @param *symbol1 first symbol
+ * @param *symbol2 second symbol
+ */
+void generate_operation(instructions_t instruction, char *result, char *symbol1, char *symbol2);
+
+/**
+ * @brief function for choosing an arithmetic operation using a switch case
+ *        ADDS/SUBS/MULS/DIVS/IDIVS are stack versions of ADD/SUB/MUL/DIV/IDIV
+ * @param *operation stack operation
+ * @param *result result
+ * @param *symbol1 first symbol
+ * @param *symbol2 second symbol
+ */
+generate_stack_operation(stack_instructions_t
+instruction,
+char *result,
+char *symbol1,
+char *symbol2
+);
+
+/**
+ * @brief function for choosing a stack relational operation using a switch case
+ *        CONCAT - concatenates symbol1 and symbol2 and stores the result in result
+ *        STRLEN - gets the length of symbol1 and stores the result in result
+ *        GETCHAR - gets the character at position symbol2 from symbol1 and stores the result in result
+ *        SETCHAR - sets the character at position symbol2 from symbol1 to symbol3
+ *        LT  - compares for lowness symbol1 and symbol2 and stores the result in result
+ *        GT  - compares for greatness symbol1 and symbol2 and stores the result in result
+ *        EQ  - compares for equality symbol1 and symbol2 and stores the result in result
+ *        NOTLT/NOTGT/NOTEQ - negation of LT/GT/EQ
+ * @param *operation stack relational operation
+ * @param *result result
+ * @param *symbol1 first symbol
+ * @param *symbol2 second symbol
+ * @param *symbol3 third symbol
+ */
+void generate_string_relational_operation(instructions_t instruction, char *result, char *symbol1, char *symbol2);
+
+/**
+ * @brief generate an integer to decimal conversion
+ */
+void generate_int_to_float();
+
+/**
+ * @brief generate a decimal to integer conversion
+ */
+void generate_float_to_int();
+
+/**
+ * @brief generate a decimal to string conversion
+ */
+void generate_int_to_char();
+
+/**
+ * @brief generate a string to decimal conversion
+ */
+void generate_string_to_int();
+
+/**
+ * @brief function for choosing an conversion operation using a switch case
+ */
+void generate_conversion_operation(stack_conversion_t operation);
+
+/**
+ * @brief read string from standard input
+ */
+void generate_reads();
+
+/**
+ * @brief read int from standard input
+ */
+void generate_readi();
+
+/**
+ * @brief read float from standard input
+ */
+void generate_readf();
+
+/**
+ * @brief print value to standard output
+ */
+void generate_write();
+
+/**
+ * @brief own substring function
+ */
+void generate_substr();
+
+/**
+ * @brief generate end of code
+ */
+void generate_end();
 
 #endif //IFJ_PROJ_2022_CODE_GENERATOR_H
