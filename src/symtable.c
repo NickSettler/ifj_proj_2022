d386bb6f78e2fa805133f3f679251029042367d1
lenamarochkina <marochkina.lena@gmail.com>
2022-10-28 14:42:35 +0200

    (refactor) implements work with token

--- src/symtable.c
+++ src/symtable.c
@@ -5,84 +5,84 @@
  *
  * @file bvs.c
  * @brief Binary search tree
- * @date 21.10.2022
+ * @date 28.10.2022
  */
 
 #include "symtable.h"
 
-tree_node *create_node(char *key) {
+tree_node *create_node(lexical_token_t *token) {
     tree_node *result = (tree_node *) malloc(sizeof(tree_node));
     if (result == 0) {
         INTERNAL_ERROR("Malloc for BST failed");
     }
     result->left = NULL;
     result->right = NULL;
-    result->key = key;
+    result->key = token->value;
 
     return result;
 }
 
-int comparator(tree_node *root, char* key){
-    if(strcmp(root->key, key) == 0){
+int comparator(tree_node *root, lexical_token_t *token){
+    if(strcmp(root->key, token->value) == 0){
         return 0;
     }
-    if(strcmp(root->key, key) > 0){
+    if(strcmp(root->key, token->value) > 0){
         return -1;
     }
     return 1;
+
 }
 
-bool insert_element(tree_node **rootptr, char *key) {
+bool insert_element(tree_node **rootptr, lexical_token_t *token) {
     tree_node *root = *rootptr;
     if (root == NULL) {
         //tree empty
-        (*rootptr) = create_node(key);
+        (*rootptr) = create_node(token);
         return true;
     }
-    switch (comparator(root, key)) {
+    switch (comparator(root, token)) {
         case 0:
             return false;
         case -1:
-            return insert_element(&(root->left), key);
+            return insert_element(&(root->left), token);
         case 1:
-            return insert_element(&(root->right), key);
+            return insert_element(&(root->right), token);
     }
 }
 
-bool find_element(tree_node *root, char *key) {
+tree_node *find_element (tree_node *root, lexical_token_t *token) {
     if (root == NULL) {
-        return false;
+        return NULL;
     }
-    switch (comparator(root, key)) {
+    switch (comparator(root, token)) {
         case 0:
-            return true;
+            return root;
         case -1:
-            return find_element(root->left, key);
+            return find_element(root->left, token);
         case 1:
-            return find_element(root->right, key);
+            return find_element(root->right, token);
     }
 }
 
-bool delete_element(tree_node **rootptr, char *key) {
+bool delete_element(tree_node **rootptr, lexical_token_t *token) {
     tree_node *root = *rootptr;
-
     if (root == NULL) {
         return false;
     }
-    switch (comparator(root, key)) {
+    switch (comparator(root, token)) {
         case 0:
             if (root->left == NULL && root->right == NULL) {
                 free(root);
-                *rootptr = NULL;
+                (*rootptr) = NULL;
                 return true;
             }
             if (root->left == NULL) {
-                *rootptr = root->right;
+                (*rootptr) = root->right;
                 free(root);
                 return true;
             }
             if (root->right == NULL) {
-                *rootptr = root->left;
+                (*rootptr) = root->left;
                 free(root);
                 return true;
             }
@@ -91,14 +91,15 @@ bool delete_element(tree_node **rootptr, char *key) {
                 temp = temp->left;
             }
             root->key = temp->key;
-            return delete_element(&(root->right), temp->key);
+            token->value = temp->key;
+            return delete_element(&(root->right), token);
         case -1:
-            return delete_element(&(root->left), key);
+            return delete_element(&(root->left), token);
         case 1:
-            return delete_element(&(root->right), key);
-
+            return delete_element(&(root->right), token);
     }
 }
+
 
 void printtabs(int numtabs) {
     for (int i = 0; i < numtabs; i++) {
--- src/symtable.h
+++ src/symtable.h
@@ -5,7 +5,7 @@
  *
  * @file symtable.h
  * @brief Binary search tree
- * @date 21.10.2022
+ * @date 28.10.2022
  */
 
 #ifndef IFJ_PROJ_2022_SYMTABLE_H
@@ -13,6 +13,7 @@
 
 #endif //IFJ_PROJ_2022_SYMTABLE_H
 #include "errors.h"
+#include "lexical_fsm.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -26,42 +27,42 @@ typedef struct tree_node {
 
 /**
  * Creates tree node with key
- * @param key identifier in the node
+ * @param pointer to the token
  * @return pointer to the tree node
  */
-tree_node *create_node(char *key);
+tree_node *create_node(lexical_token_t *token);
 
 /**
- * Compare key with key in the node
+ * Compare token value with key in the node
  * @param root pointer to the node
- * @param key value to compare
+ * @param pointer to the token, which value is compared with key
  * @return 0 if values are equal, -1 if value in the node is greater, 1 if value in the node is smaller
  */
-int comparator(tree_node *root, char *key);
+int comparator(tree_node *root, lexical_token_t *token);
 
 /**
  * Inserts value into the tree
  * @param rootptr pointer to the pointer to the root of the tree
- * @param value value to insert
- * @return true if value was inserted, false if value already exists in the tree
+ * @param pointer to the token, which value will be inserted
+ * @return true if key was inserted, false if key already exists in the tree
  */
-bool insert_element(tree_node **rootptr, char *key);
+bool insert_element(tree_node **rootptr, lexical_token_t *token);
 
 /**
  * Finds value in the tree
  * @param root pointer to the root of the tree
- * @param value value to find
- * @return true if value was found, false if value was not found
+ * @param pointer to the token, which value is searched
+ * @return  if key was found
  */
-bool find_element(tree_node *root, char *key);
+tree_node *find_element (tree_node *root, lexical_token_t *token);
 
 /**
  * Deletes value from the tree
  * @param rootptr pointer to the pointer to the root of the tree
- * @param value value to delete
+ * @param pointer to the token, which value will be deleted
  * @return true if value was deleted, false if value was not found
  */
-bool delete_element(tree_node **rootptr, char *key);
+bool delete_element(tree_node **rootptr, lexical_token_t *token);
 
 //next 2 functions only for testing
 void printtabs(int numtabs);
