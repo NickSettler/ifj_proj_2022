b6da964f321a91b526e0acad3e9e49508021e835
NickSettler <moiseevnikita14@gmail.com>
2022-11-24 20:12:06 +0100

    (feat) changes main to read program from stdin

--- src/main.c
+++ src/main.c
@@ -7,8 +7,31 @@
  * @brief Main file
  * @date 24.09.2022
  */
+#include "symtable.h"
+#include "syntax_analyzer.h"
+#include "semantic_analyzer.h"
+#include "code_generator.h"
 
 int main(int argc, char **argv) {
+    FILE *input = stdin;
+
+    syntax_abstract_tree_t *tree = load_syntax_tree(input);
+
+    semantic_tree_check(tree);
+
+    set_code_gen_output(stdout);
+    code_generator_init();
+    generate_header();
+    parse_tree(tree);
+    generate_exit(0);
+
+    parse_func_dec(tree);
+    generate_number_conversion_functions();
+    generate_substr();
+    generate_ord();
+
+    dispose_symtable();
+//    free_syntax_tree(tree);
 
     return 0;
 }
